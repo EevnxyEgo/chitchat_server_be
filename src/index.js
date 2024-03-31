ef3572b014f980e0bb1ac47dd53854ac8def439d
EevnxyEgo <arseniuswahyu@gmail.com>
2024-04-01 01:45:34 +0700

    setup backend done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
-
+import mongoose from "mongoose";
 
 //env variables
+const {DATABASE_URL} = process.env;
 const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
+
+//mongodb error
+mongoose.connection.on('error', (err)=>{
+    logger.error(`Mongodb connection error: ${err}`);
+    process.exit(1);
+});
+
+//mongodb debug mode
+if(process.env.NODE_ENV !== 'production'){
+    mongoose.set("debug", true);
+}
+
+//mongodb
+mongoose.connect(DATABASE_URL,{}).then(()=>{
+    logger.info('Connected to Database.');
+});
+
 
 let server;
 
 server = app.listen(PORT, ()=> {
-    logger.info(`Server is listening at ${PORT}... `)
-    console.log("process id", process.pid)
+    logger.info(`Server is listening at ${PORT}... `);
+    console.log("process id", process.pid);
 });
 
 
