7c565c99e1eab94476c3c42c9c82eb2dc987e214
EevnxyEgo <arseniuswahyu@gmail.com>
2024-03-31 15:22:06 +0700

    add http-errors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,35 @@ import logger from "./configs/logger.config.js";
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV)
 
-app.listen(PORT, ()=> {
+let server;
+
+server = app.listen(PORT, ()=> {
     logger.info(`Server is listening at ${PORT}... `)
+    console.log("process id", process.pid)
+});
+
+
+//handle server errors
+const exitHandler= () => {
+    if(server){
+        logger.info("Server closed.");
+        process.exit(1);
+    }else{
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler= (error) => {
+    logger.error(error);
+    exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+    if(server){
+        logger.info("Server closed.");
+        process.exit(1);
+    }
 });
